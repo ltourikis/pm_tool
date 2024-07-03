bd68f3271910e331ae1d5ff97a4495d3dbc75432
tourikis <tourikis3@hotmail.com>
2024-07-03 13:00:11 +0300

    Change log file name to log.log

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('test.log'),
+        logging.FileHandler('log.log'),
         logging.StreamHandler()
     ]
 )
